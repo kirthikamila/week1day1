8d1de7ec7e922622ff34fa13b5610775a955bf0f
kirthikamila <rajukirthika31@gmail.com>
2025-04-30 20:42:05 +0530

    Add files via upload

---
+++ Learning1day.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Learning1day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float value=91.0f;
+				System.out.println(value);
+		
+		String developer="Google";
+				System.out.println(developer);
+			
+		boolean Beta=false;
+		        System.out.println(Beta);
+		
+		int release=2008;
+		        System.out.println(release);
+		       
+		char shortcutkey='c';
+		        System.out.println(shortcutkey);
+		
+			
+	}
+
+}
